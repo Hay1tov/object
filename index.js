5d05308001a127a036c9ec2da8c3e9944b1f535c
Hay1tov <eldorh35@gmail.com>
2025-08-28 11:56:04 +0500

    update

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict'
+//                                                    No_1
+
+// const me = {
+//   firstName: "Abdulaziz",
+//   lastName: "Toshpulatov",
+//   age: 23,
+//   languages: ["js", "python", "c++", "nodejs"],
+//   friends: ["Jamshid", "Abbos", "Jalol", "Mar'uf"],
+// };
+
+// function formatObject3(obj) {
+//   return Object.entries(obj).reduce((acc, [key, value]) => {
+//     if (Array.isArray(value)) {
+//       return acc + `${key}: ${key === "languages" ? value.join(",") : value.join("+")}\n`;
+//     }
+//     return acc + `${key}: ${value.toString()}\n`;
+//   }, "").trim();
+// }
+
+// console.log(formatObject3(me));
+
+
+//                                                    No_2
+
+
+// function getSquare(n) {
+//   let square = {}
+
+//   for(let i = 1; i <= n; i++) {
+//     square[i] = i**2
+//   }
+//   return square
+// }
+
+// console.log(getSquare(5));
+
+
+//                                                    No_3
+
+
+function getSquare(n) {
+  let square = {}
+
+  for(let i = 1; i <= n; i++) {
+    square[i] = i**2
+  }
+  return square
+}
+
+function sumKeysAndValues(square) {
+  
+}
+
+
+//                                                    No_4
+//                                                    No_5
+//                                                    No_6
+//                                                    No_7
+//                                                    No_8
+//                                                    No_9
+//                                                    No_10
+//                                                    No_11
+//                                                    No_12
+//                                                    No_13
+//                                                    No_14
+//                                                    No_15
